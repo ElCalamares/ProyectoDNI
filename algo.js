d2ea5be82895256b3716ece6d6e4da4ec0fed432
Rodrigo <rodrigo.canand@educa.jcyl.es>
2024-10-07 22:58:16 +0200

    third commit

--- algo.js
+++ algo.js
@@ -1,90 +1,18 @@
-let dni="72377566Q";//prompt("Dame tu DNI por favor");
-let dniInt="";
-let letra="";
-for (let i = 0; i < dni.length-1; i++) {
-    dniInt+=dni[i];
-}
-switch(dniInt%23){
-    case 0:
-        letra="T";
-        break;
-    case 1:
-        letra="R";
-        break;
-    case 2:
-        letra="W";
-        break;
-    case 3:
-        letra="A";
-        break;
-    case 4:
-        letra="G";
-        break;
-    case 5:
-        letra="M";
-        break;
-    case 6:
-        letra="Y";
-        break;
-    case 7:
-        letra="F";
-        break;
-    case 8:
-        letra="P";
-        break;
-    case 9:
-        letra="D";
-        break;
-    case 10:
-        letra="X";
-        break;
-    case 11:
-        letra="B";
-        break;
-    case 12:
-        letra="N";
-        break;
-    case 13:
-        letra="J";
-        break;
-    case 14:
-        letra="Z";
-        break;
-    case 15:
-        letra="S";
-        break;
-    case 16:
-        letra="Q";
-        break;
-    case 17:
-        letra="V";
-        break;
-    case 18:
-        letra="H";
-        break;
-    case 19:
-        letra="L";
-        break;
-    case 20:
-        letra="C";
-        break;
-    case 21:
-        letra="K";
-        break;
-    case 22:
-        letra="E";
-        break;
-    default:
-        letra=-1;
-        break;
-}
-if(letra==-1){
-    console.log("Algo salio mal");
-}else{
-    console.log(letra);
-    if(letra==dni[8]){
-        console.log("Tu DNI esta bien")
-    }else{
-        console.log("Esta mal");
+//"72377566Q";
+function comprobarDni(dni){
+    let dniInt = "";
+    let letras = ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'];
+    for (let i = 0; i < dni.length - 1; i++) {
+        dniInt += dni[i];
+    }
+    let respuesta = "";
+    if (letras[dniInt % 23] == dni[8]) {
+        respuesta = "Tu DNI está bien"; 
+    } else {
+        respuesta = "Está mal";
     } 
+    console.log(respuesta);
+    return respuesta;
 }
+
+
